1322f86268746027463f4a0f1e6f8e559b0c681f
Mohammed Aboutalha <mohammed.aboutalha.2019@gmail.com>
2021-12-14 20:12:29 +0000

    add icon cloud

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -6,9 +6,8 @@
 var on = "yes";
 const but=document.getElementById("bx-help-block");
 but.addEventListener("click",function(){
-    if (on === "yes") {
+        if (on === "yes") {
         openNav();
-     
         document.getElementById("body").style.overflowY="hidden";
         document.getElementById("bx-im-bar").style.borderRight="17px solid rgb(238, 242, 244)";
         document.getElementById("bx-im-bar").classList.add("bx-im-bar-default");
@@ -16,14 +15,13 @@ but.addEventListener("click",function(){
         }
         else{ 
         closeNav();
-
         document.getElementById("body").style.overflowY="scroll";
         document.getElementById("bx-im-bar").style.borderRight="0 solid rgb(238, 242, 244)";
+        document.getElementById("bx-im-bar").style.transition="0.3s";
+        document.getElementById("loading-image").style.transition="0.3s";
         document.getElementById("bx-im-bar").classList.remove("bx-im-bar-default");        
         on = "yes";
-
         }
-
 });
 function openNav() {
     document.getElementById("mySidenav").style.width = "75%";}
