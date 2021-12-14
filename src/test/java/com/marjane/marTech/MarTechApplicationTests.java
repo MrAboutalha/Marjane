3ed6cae749dcb100a9ec38c37bbc043d5cdc5bac
Mohammed Aboutalha <mohammed.aboutalha.2019@gmail.com>
2021-12-14 11:03:44 +0100

    Initial Commit

---
+++ src/test/java/com/marjane/marTech/MarTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marjane.marTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
