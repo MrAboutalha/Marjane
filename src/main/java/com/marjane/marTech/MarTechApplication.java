b4e1b18fe3d8d3837755a5cce9eea282ce8b3554
Mohammed Aboutalha <mohammed.aboutalha.2019@gmail.com>
2021-12-14 11:48:38 +0100

    Initial Commit

---
+++ src/main/java/com/marjane/marTech/MarTechApplication.java
@@ -0,0 +1,13 @@
+package com.marjane.marTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarTechApplication.class, args);
+	}
+
+}
