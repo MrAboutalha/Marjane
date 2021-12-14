60c55c56e666c631e8310aa37bac8316f23f0ac6
Mohammed Aboutalha <mohammed.aboutalha.2019@gmail.com>
2021-12-14 19:03:14 +0000

    Menu Looks like the original

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -8,15 +8,24 @@ const but=document.getElementById("bx-help-block");
 but.addEventListener("click",function(){
     if (on === "yes") {
         openNav();
+     
+        document.getElementById("body").style.overflowY="hidden";
+        document.getElementById("bx-im-bar").style.borderRight="17px solid rgb(238, 242, 244)";
+        document.getElementById("bx-im-bar").classList.add("bx-im-bar-default");
         on = "no";
         }
         else{ 
         closeNav();
+
+        document.getElementById("body").style.overflowY="scroll";
+        document.getElementById("bx-im-bar").style.borderRight="0 solid rgb(238, 242, 244)";
+        document.getElementById("bx-im-bar").classList.remove("bx-im-bar-default");        
         on = "yes";
+
         }
 
 });
 function openNav() {
-    document.getElementById("mySidenav").style.width = "750px";}
+    document.getElementById("mySidenav").style.width = "75%";}
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";}
